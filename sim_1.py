356742e3fb26c756ec8b6be99ce37fbaf0f1b340
CameronB66 <feed@cmbain.com>
2020-07-21 18:20:08 +0100

    initial work

---
+++ sim_1.py
@@ -0,0 +1,146 @@
+#========== Define Track Classes ============#
+
+class Straight:
+	def __init__(self, length):
+		self.length = length #1 2 3 - short medium long
+		self.type = "STRAIGHT"
+		self.next = None
+
+	def add_next(self,next):
+		self.next = next
+
+	def next(self):
+		return self.next
+
+class Turn:
+	def __init__(self, sev, length):
+		self.sev = sev #1 2 3 - slow medium fast
+		self.length = length #1 2 3 - short medium long
+		self.type = "TURN"
+		self.next = None
+
+	def add_next(self, next):
+		self.next = next
+
+	def next(self):
+		return self.next
+
+class Start_finish:
+	def __init__(self):
+		self.next = None
+
+class Track:
+	def __init__(self, name):
+		self.name = name
+		self.start_finish = Start_finish()
+		self.track_arr = [self.start_finish]
+
+	def add_straight(self, length):
+		new_straight = Straight(length)
+		new_straight.next = self.start_finish
+		self.track_arr[-1].next = new_straight
+		self.track_arr.append(new_straight)
+
+	def add_turn(self, sev, lengthh)
+		new_turn = Turn(sev, length)
+		new_turn.next = self.start_finish
+		self.track_arr[-1].next = new_turn
+		self.track_arr.append(new_turn)
+
+#========== Define Driver Classes ===========#
+
+
+
+
+#========== Define Car Classes =============#
+class Car:
+	def __init__(self, traction, top_speed, downforce) #add driver to car here
+		self.traction = traction #float 0-1
+		self.top_speed = top_speed #float 0-1
+		self.downforce = downforce #float 0-1
+		self.tyre_wear = None
+		self.tyre_type = None
+		self.damage = 1.0
+
+	def set_tyre(tyre):
+		self.tyre_type = tyre # 1 2 3 - hard medium soft
+		self.tyre_wear = 1.0
+
+	def repair():
+		repair_val = 1-self.damage
+		self.damage = 1.0
+		return repair_val
+
+
+#========= Define Race Classes ===============#
+class Race:
+	def __init__(self, track, laps):
+		self.sim = Sim()
+		self.track = track
+		self.laps = laps
+		self.race_tracker = {}
+		self.num_cars = 0
+
+	def add_car(car):
+		self.num_cars += 1
+		self.cars[self.num_cars] = {"car":car, "gap":0.5, "speed_mod":0, "downforce_mod":0}
+		self.cars[1]["gap"] = None
+
+	
+
+
+#========== Simulation Settings =============#
+
+#For now just hard code it all here
+class Sim:
+	def __init__(self):
+		self.max_repair = 30 #repair time = damage amount * max repair
+
+		self.slip_close = 0.2 #speed bonus for close slipstream
+		self.slip_far = 0.1 #speed bonus for further slip stream
+		self.dirty_air_close = -0.2 #downforce penalty for close dirty air
+		self.dirty_air_far = -0.1 #downforce penalty for far dirty air
+		
+		self.traction_tyre_wear_coefficient = 1 #traction = traction_val * tyre_wear * coefficient
+		self.max_traction_diff = 0.75 #max time that can be lost through lack of traction
+
+	def calculate_modifiers(race_tracker):
+		num_cars = len(race_tracker)
+		for index in range(0,num_cars):
+			place = index+1
+			sp_mod = 0
+			down_mod = 0
+			if race_tracker[place]["gap"] < 0.5:
+				sp_mod = self.slip_close
+				down_mod = self.dirt_air_close				
+
+			elif race_tracker[place]["gap"] < 1:
+				sp_mod = self.slip_far
+				down_mod = self.dirty_air_far
+
+			race_tracker[place]["speed_mod"] = sp_mod
+			race_tracker[place]["downforce_mod"] = down_mod
+
+	def calc_traction_zone(race_tracker): #passing dicts from race_tracker
+		num_cars = len(race_tracker)
+		for index in range(1,num_cars):
+			place = index+1
+			car_ahead = race_tracker[index]["car"]
+			car_behind = race_tracker[place]["car"]
+
+			traction_diff = ( car_behind.traction*car_behind.tyre_wear - car_ahead.traction*car_ahead.tyre_wear)*self.traction_tyre_wear_coefficient
+			time_diff = traction_diff*self.max_traction_diff
+
+			race_tracker[place]["gap"]+=time_diff
+
+
+
+
+			
+
+
+
+
+
+
+
